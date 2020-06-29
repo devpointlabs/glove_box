98ff67117e56f887bbb4d9bdfa3fb055e6d5e6ce
Max Sultan <maxsultakent@gmail.com>
2020-06-29 16:25:19 -0600

    added routes for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+
+  namespace :api do
+    resources :users do 
+      resources :vehicles do
+        resources :records
+      end
+    end
+  end
 end
