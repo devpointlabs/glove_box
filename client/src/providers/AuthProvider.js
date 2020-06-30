50ccc80ef96127112cb589c8e924c825b443f37c
Andre Leveille <andre.joe.leveille@gmail.com>
2020-06-30 16:04:02 -0600

    fixed migration files

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 export const AuthContext = React.createContext()
 export const AuthConsumer = AuthContext.Consumer
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,71 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_29_220125) do
+ActiveRecord::Schema.define(version: 2020_07_29_220126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "records", force: :cascade do |t|
+    t.datetime "date"
+    t.string "maintenance_task"
+    t.integer "mileage"
+    t.string "image"
+    t.string "category"
+    t.string "reciepts"
+    t.bigint "vehicle_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["vehicle_id"], name: "index_records_on_vehicle_id"
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email"
+    t.json "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "username"
+    t.integer "sign_in_count", default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  create_table "vehicles", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.integer "year"
+    t.string "color"
+    t.string "type"
+    t.string "vin"
+    t.boolean "roadside_assistance"
+    t.string "insured_by"
+    t.string "policy_number"
+    t.string "image"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_vehicles_on_user_id"
+  end
+
+  add_foreign_key "records", "vehicles"
 end
