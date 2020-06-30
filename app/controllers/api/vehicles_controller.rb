eb93c5aacb50594c5538f55eea6ef4c46da5664e
Max Sultan <maxsultakent@gmail.com>
2020-06-30 15:16:59 -0600

    added CRUD actions to vehicles controller

--- app/controllers/api/vehicles_controller.rb
+++ app/controllers/api/vehicles_controller.rb
@@ -5,12 +5,24 @@ class Api::VehiclesController < ApplicationController
     end 
 
     def create
+        vehicle = current_user.vehicles.new(vehicle_params)
+        if vehicle.save
+            render json: vehicle
+        else
+            render json: {errors: vehicle, status: 422}
+        end 
     end 
 
     def update
+        if @vehicle.update
+            render json: @vehicle
+        else
+            render json: {errors: @vehicle.errors, status: 422}
+        end 
     end 
 
     def destroy
+        render json: @vehicle.destroy
     end 
 
 
