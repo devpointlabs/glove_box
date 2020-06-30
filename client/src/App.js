25b07e73030f93d493094badf4d6eb8c22b8c4ec
Max Sultan <maxsultakent@gmail.com>
2020-06-30 16:05:49 -0600

    added a login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
 import Register from './Register';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Register/>
+      <Navbar/>
+      <switch>
+        <Register/>
+        <Login/>
+      </switch>
     </div>
   );
 }
