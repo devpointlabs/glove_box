f45416199cdeba40ba2e51c7c95714ee3397cc28
Max Sultan <maxsultakent@gmail.com>
2020-07-01 17:02:14 -0600

    finished the view for the dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Navbar';
 import Register from './Register';
 import Login from './Login';
+import Dashboard from './Dashboard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <switch>
         <Register/>
         <Login/>
+        <Dashboard/>
       </switch>
     </div>
   );
--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -2,15 +2,22 @@ import React from 'react'
 
 export default function Dashboard() {
 
-    defaultImage = 'https://lh3.googleusercontent.com/proxy/pBC2-MR-kzg4iZ5V20cIW587JBKPTLsVmJdOn2_r0uGz4HW9-RydVgiBArCoFJNkbErN3vhINBcG6ig0TsicqMSbZeLno75L_7-423rpeZA56mhgSMtzqRWi'
+    const defaultImage = 'https://lh3.googleusercontent.com/proxy/pBC2-MR-kzg4iZ5V20cIW587JBKPTLsVmJdOn2_r0uGz4HW9-RydVgiBArCoFJNkbErN3vhINBcG6ig0TsicqMSbZeLno75L_7-423rpeZA56mhgSMtzqRWi'
     
     return (
-        <div>
+        <div style={styles.page}>
             <div style={styles.contain}>
-                <h1>Dashboard</h1>
+                <h1 style={{textAlign:'left', fontSize:'50px'}}>Dashboard</h1>
 
-                <h3>CAR DETAILS</h3>
-                <div>{defaultImage}<div></div></div>
+                <h3 style={{textAlign:'left', color:'#A7AAB2', fontSize:'30px', marginBottom:'30px'}}>CAR DETAILS</h3>
+                <div style={styles.card}>
+                    <img src={defaultImage} style={{width:'50%'}}/>
+                    <div style={styles.right}>
+                        <h1 style={{fontSize:'40px'}}><strong>Add Title Here</strong></h1>
+                        <h3>Unlock your vehicles estimated trade-in value and up-to-date recalls when you add your liscense plate.</h3>
+                        <button style={styles.buttonStyle}><strong>ADD LISCENSE PLATE</strong></button>
+                    </div>
+                </div>
             </div>
         </div>
     )
@@ -18,6 +25,42 @@ export default function Dashboard() {
 
 const styles = {
     contain: {
-        maxWidth: '60%'
-    }
+        maxWidth: '60%',
+        
+    },
+     page: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F7F7F7',
+     },
+     card: {
+         minHeight: '400px',
+        width:'90%',
+        display: 'flex',
+        border: '2px solid white',
+        borderRadius: '10px',
+        boxShadow: '1px 1px 6px 5px #eeeeee'
+     },
+     right: {
+         display: 'flex',
+         flexDirection: 'column',
+         justifyContent:'space-around',
+         minWidth: '45%',
+         maxWidth:'50%',
+         padding: '20px 60px',
+         backgroundColor: 'white',
+     },
+     buttonStyle: {
+         all: 'unset',
+         maxWidth: '50%',
+         padding: '20px 15px',
+         color: 'white',
+         backgroundColor: 'black',
+         borderRadius: '5px',
+         marginLeft: 'auto',
+         marginRight: 'auto',
+     }
 }
