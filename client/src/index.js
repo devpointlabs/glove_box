bc01affb7ad1e4021835676dac63b697251063c8
Andre Leveille <andre.joe.leveille@gmail.com>
2020-07-01 14:54:20 -0600

    auth provider and fetch user

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AuthProvider from './providers/AuthProvider';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <AuthProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AuthProvider>,
   document.getElementById('root')
 );
 
