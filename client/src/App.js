02f1e4c5ad2d5080634df264008f711ed2de002f
Andre Leveille <andre.joe.leveille@gmail.com>
2020-07-01 16:48:29 -0600

    updated navbar and created protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import Navbar from './Navbar';
 import Register from './Register';
 import Login from './Login';
+import FetchUser from './components/FetchUser'
+import { Switch, Route, } from 'react-router-dom';
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
-      <switch>
-        <Register/>
-        <Login/>
-      </switch>
+      <FetchUser>
+        <Switch>
+          <Register/>
+          {/* <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+          <ProtectedRoute exact path='/documents' component={Documents} />
+          <ProtectedRoute exact path='/profile' component={Profile} /> */}
+          <Route exact path='/login' component={Login}/>
+          {/* <Route exact path='/contact_us' component={ContactUs}/> */}
+        </Switch>
+      </FetchUser>
     </div>
   );
 }
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,32 +1,100 @@
 import React from 'react'
-import {Menu} from 'semantic-ui-react'
+import { AuthConsumer, } from "./providers/AuthProvider";
+import { Menu, } from 'semantic-ui-react'
+import { Link, withRouter, } from 'react-router-dom'
 
-export default function Navbar() {
+class Navbar extends React.Component {
 
-    const authenticated = true
+  rightNavItems = () => {
+    const { auth: { user, handleLogout, }, location, } = this.props;
 
-    const rightMenuItems = () => {
-        if(authenticated){
-            return(
-                <Menu.Menu position='right'>
-                    <Menu.Item name='logout'/>
-                </Menu.Menu>
-            )
-        }else{
-           return( <Menu.Menu position='right'>
-                        <Menu.Item name='login'/>
-                        <Menu.Item name='register'/>
-                    </Menu.Menu>
-           )
-        }
+    if (user) {
+      return (
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='logout'
+            onClick={() => handleLogout(this.props.history)}
+          />
+          <Link to='/contact_us'>
+            <Menu.Item
+              id='contact us'
+              name='contact us'
+              active={location.pathname === '/contact_us'}
+            />
+          </Link>
+        </Menu.Menu>
+      )
+    } else {
+      return (
+        <Menu.Menu position='right'>
+          <Link to='/login'>
+            <Menu.Item
+              id='login'
+              name='login'
+              active={location.pathname === '/login'}
+            />
+          </Link>
+          {/* <Link to='/register'>
+            <Menu.Item
+              id='register'
+              name='register'
+              active={location.pathname === '/register'}
+            />
+          </Link> */}
+          <Link to='/contact_us'>
+            <Menu.Item
+              id='contact us'
+              name='contact us'
+              active={location.pathname === '/contact_us'}
+            />
+          </Link>
+        </Menu.Menu>
+      )
     }
+  }
 
+  render() {
     return (
-      <Menu>
-          <Menu.Item name='home'/>
-          <Menu.Item name='my vehicles'/>
-          <Menu.Item name='my records'/>
-          {rightMenuItems()}
-      </Menu>
-    )
-}
+      <div>
+        <Menu pointing secondary>
+          <Link to='/'>
+            <Menu.Item
+              name='dashboard'
+              id='dashboard'
+              active={this.props.location.pathname === '/'}
+            />
+          </Link>
+          <Link to='/documents'>
+            <Menu.Item
+              name='documents'
+              id='documents'
+              active={this.props.location.pathname === '/documents'}
+            />
+          </Link>
+          <Link to='/profile'>
+            <Menu.Item
+              name='profile'
+              id='profile'
+              active={this.props.location.pathname === '/profile'}
+            />
+          </Link>
+          { this.rightNavItems() }
+        </Menu>
+      </div>
+    );
+  };
+};
+
+export class ConnectedNavbar extends React.Component {
+  render() {
+    return (
+      <AuthConsumer>
+        { auth =>
+          <Navbar {...this.props} auth={auth} />
+        }
+      </AuthConsumer>
+    );
+  };
+};
+
+export default withRouter(ConnectedNavbar);
