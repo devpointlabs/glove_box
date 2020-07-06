4241461617f02b484bb7848879cad9966218fb2f
Andre Leveille <andre.joe.leveille@gmail.com>
2020-07-06 15:25:47 -0600

    converting to bootstrap

--- client/src/Register.js
+++ client/src/Register.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Button, Form, Segment, Header } from "semantic-ui-react";
+// import { Button, Form, Segment, Header } from "semantic-ui-react";
 import { AuthConsumer } from "./providers/AuthProvider";
+import {Form, Card, Button} from 'react-bootstrap'
+
 
 class Register extends React.Component {
-  state = {email:'', password:'', passwrodConfirmation:''}
+  state = {email:'', password:'', passwordConfirmation:'', fname:'', lname:''}
 
   handleSubmit = (e) => {
     const {auth: {handleRegister}, history} = this.props
@@ -19,62 +21,145 @@ class Register extends React.Component {
     this.setState({[name]: value})
   }
   render() {
-    const { email, password, passwordConfirmation } = this.state;
+    const { email, password, passwordConfirmation, fname, lname } = this.state;
 
     return (
-      <Segment basic>
-        <Header as="h1" textAlign="center">
-          Welcome to your Digital GloveBox
-        </Header>
-        <Header as="p" textAlign="center">
-        A place for you to store all of your important vehicle records
-        </Header>
+        <>
         <br/>
-        <Form as='h2' onSubmit={this.handleSubmit}>
-            Sign up for a free account
-          <Form.Input
-            label="Name"
-            required
-            autoFocus
-            name="name"
-            value={email}
-            placeholder="Name"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Email"
-            required
-            autoFocus
-            name="email"
-            value={email}
-            placeholder="Email"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Password"
-            required
-            name="password"
-            value={password}
-            placeholder="Password"
-            type="password"
-            onChange={this.handleChange}
-          />
-          <Form.Input
-            label="Password Confirmation"
-            required
-            name="passwordConfirmation"
-            value={passwordConfirmation}
-            placeholder="Password Confirmation"
-            type="password"
-            onChange={this.handleChange}
-          />
-          <Segment textAlign="center" basic>
-            <Button primary type="submit">
-              Submit
-            </Button>
-          </Segment>
-        </Form>
-      </Segment>
+        <h1>Welcome to your Digital GloveBox</h1>
+        <p>A place for you to store all of your important vehicle records</p>
+        <br/>
+        <br/>
+
+        <Card style ={{width: '36rem'}}>
+            <Card.Body>
+            <Form>
+                <Form.Row>
+                    <Form.Group  controlId="formGridFirstName">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control 
+                        autoFocus
+                        required
+                        type="text" 
+                        placeholder="First Name" 
+                        label="Name"
+                        name="name"
+                        value={window.name}
+                        onChange={this.handleChange}
+                    />
+                    </Form.Group>
+
+                    <Form.Group controlId="formGridLastName">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control 
+                        required
+                        type="text" 
+                        placeholder="Last Name" 
+                    />
+                    </Form.Group>
+                </Form.Row>   
+
+                <Form.Group controlId="formGroupEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control 
+                        label="Email"
+                        required
+                        name="email"
+                        value={email}
+                        placeholder="Email"
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="formGroupPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control 
+                        label="Password"
+                        required
+                        name="password"
+                        value={password}
+                        placeholder="Password"
+                        type="password"
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Form.Group controlId="formGrouppasswordConfirmation">
+                    <Form.Label>Confirm Password</Form.Label>
+                    <Form.Control 
+                        label="Password Confirmation"
+                        required
+                        name="passwordConfirmation"
+                        value={passwordConfirmation}
+                        placeholder="Re-type Password"
+                        type="password"
+                        onChange={this.handleChange}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Sign Up
+                </Button>
+            </Form>
+            </Card.Body>
+        </Card>
+        </>
+
+
+
+
+    //   <Segment basic>
+    //     <Header as="h1" textAlign="center">
+    //       Welcome to your Digital GloveBox
+    //     </Header>
+    //     <Header as="p" textAlign="center">
+    //     A place for you to store all of your important vehicle records
+    //     </Header>
+    //     <br/>
+    //     <Form as='h2' onSubmit={this.handleSubmit}>
+    //       <p>Sign up for a free account</p>
+    //       <Form.Input
+    //         label="Name"
+    //         required
+    //         autoFocus
+    //         name="name"
+    //         value={window.name}
+    //         placeholder="Name"
+    //         onChange={this.handleChange}
+    //       />
+    //       <Form.Input
+    //         label="Email"
+    //         required
+    //         name="email"
+    //         value={email}
+    //         placeholder="Email"
+    //         onChange={this.handleChange}
+    //       />
+    //       <Form.Input
+    //         label="Password"
+    //         required
+    //         name="password"
+    //         value={password}
+    //         placeholder="Password"
+    //         type="password"
+    //         onChange={this.handleChange}
+    //       />
+    //       <Form.Input
+    //         label="Password Confirmation"
+    //         required
+    //         name="passwordConfirmation"
+    //         value={passwordConfirmation}
+    //         placeholder="Password Confirmation"
+    //         type="password"
+    //         onChange={this.handleChange}
+    //       />
+    //       <Segment textAlign="center" basic>
+    //         <Button primary type="submit">
+    //           Submit
+    //         </Button>
+    //       </Segment>
+    //     </Form>
+    //   </Segment>
+
+
+
     );
   }
 }
@@ -89,21 +174,3 @@ export default class ConnectedRegister extends React.Component {
     )
   }
 }
-
-   {/* <h1>Welcome to your Digital Glovebox</h1>
-        <p>A place for you to store all of your important vehicle records</p>
-        <div style={styles.formDiv}>
-            <form title='Register' onSubmit={this.handleSubmit} style={styles.vertForm}>
-                <h2>Sign up for a free account</h2>
-                <label for='name'>Name</label>
-                <input type='text' label='name' name='name' value={name} onChange={this.handleChange}/>
-                <label for='email'>Email</label>
-                <input type='text' label='email' name='email' value={email} onChange={this.handleChange}/>
-                <label for='password'>Password</label>
-                <input type='text' label='password' name='password' value={password} onChange={this.handleChange}/>
-                <label for='passwordConf'>Password Confirmation</label>
-                <input type='text' label='passwordConfirmation' name='passwordConfirmation' value={passwordConfirmation} onChange={this.handleChange}/>
-                <button style={styles.buttonStyle}>Submit</button>
-            </form>
-        </div>
-        </> */}
--- client/src/components/Dashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Dashboard = () => (
-  <div>
-    <h1 testAlign="center" >Dashboard test</h1>
-  </div>
-)
-
-export default Dashboard
